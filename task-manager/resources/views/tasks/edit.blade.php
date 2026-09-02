29b77da9f4ba24e03e14a9f1257cb5b044dc96cc
shim-zzz <shielaamaee@gmail.com>
2026-09-02 16:49:10 +0800

    allow editing task title

--- task-manager/resources/views/tasks/edit.blade.php
+++ task-manager/resources/views/tasks/edit.blade.php
@@ -6,12 +6,16 @@
     <title>Edit task</title>
 </head>
 <body>
-    
+
     <h1>Edit Task</h1>
 
     <form action="{{ route('tasks.update', $task->id) }}" method="POST">
         @csrf
         @method('PUT')
 
+        <label>Task Title:</label>
+        <input type="text" name="title" value="{{ $task->title }}" required>
+
+        <br><br>
 </body>
 </html>
