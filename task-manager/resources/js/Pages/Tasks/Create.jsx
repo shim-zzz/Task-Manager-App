5d6770768b194343dd9fd09a7656c6d987c43f83
shim-zzz <shielaamaee@gmail.com>
2026-09-02 17:27:14 +0800

    initialize task creation form & add task form

--- task-manager/resources/js/Pages/Tasks/Create.jsx
+++ task-manager/resources/js/Pages/Tasks/Create.jsx
@@ -1,5 +1,14 @@
 import { Link, useForm } from '@inertiajs/react';
 
 export default function Create() {
+    const { data, setData, post, processing, errors } = useForm({
+        title: '',
+        description: '',
+    });
 
+    const submit = (e) => {
+        e.preventDefault();
+
+        post('/tasks');
+    };
 }
