6c0a916520b4eae9b3ef4c5c79e3531fd25f01ca
shim-zzz <shielaamaee@gmail.com>
2026-09-02 16:27:00 +0800

    Implement task deletion

--- task-manager/app/Http/Controllers/TaskController.php
+++ task-manager/app/Http/Controllers/TaskController.php
@@ -98,8 +98,13 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
-    }
+    public function destroy($id)
+{
+    $task = Task::findOrFail($id);
+
+    $task->delete();
+
+    return redirect()->route('tasks.index');
+}
+
 }
